239c326ee9ff8877dfac2d30c5696bed7eb68eec
Jace Liu <ly950710@gmail.com>
2017-05-15 17:59:24 +0800

    Modify code generator of static_ip blocks
    Signed-off-by: Jace Liu <ly950710@gmail.com>

--- generators/java/network_constants.js
+++ generators/java/network_constants.js
@@ -14,5 +14,5 @@ Blockly.Java['network_static_ip'] = function(block) {
   var IP_2 = block.getFieldValue('IP_2');
   var IP_3 = block.getFieldValue('IP_3');
   var IP_4 = block.getFieldValue('IP_4');
-  return '\"' + IP_1 + '.' + IP_2 + '.' + IP_3 + '.' + IP_4 + '\"';
+  return "IPv4.toIPv4Address(" + '\"' + IP_1 + '.' + IP_2 + '.' + IP_3 + '.' + IP_4 + "\")";
 };
